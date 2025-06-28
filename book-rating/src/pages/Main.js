8633e626eb83d94271b8f962eb5eb65920f337b6
Katerina Koller <koller.katerina@gmail.com>
2025-06-28 20:56:39 +0200

    link fix 2.0

--- book-rating/src/pages/Main.js
+++ book-rating/src/pages/Main.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import { Link } from 'react-router-dom'
+
 
 const Main = () => {
   return (
